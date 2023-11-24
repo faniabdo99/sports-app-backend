99b382a26f9712dd0beaee1376a4881c6880ce61
Abd Alrahman Fani <faniabdo99@gmail.com>
2023-11-24 23:42:06 +0200

    Updated the users test cases

--- factories/user.js
+++ factories/user.js
@@ -1,13 +1,15 @@
 import { faker } from '@faker-js/faker';
 import db from '../models/index.js';
 
-async function createUser(attributes = {}) {
-    return await db.User.create({
+function createUser(attributes = {}) {
+    return db.User.create({
         firstName: faker.person.firstName(),
         lastName: faker.person.lastName(),
         email: faker.internet.email(),
         password: faker.person.firstName(),
         phoneNumber: '01234456789'
+    }).then(user => {
+        return user;
     });
 }
 export default createUser;
--- test/api/users.js
+++ test/api/users.js
@@ -1,23 +1,27 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../../index.js';
-import createUser from '../../factories/user.js';
-import resetDatabase from '../../config/reset.js'; // Fix the spelling here
 chai.use(chaiHttp);
 const expect = chai.expect;
 
 describe('GET /users', () => {
-  it('should return a status 200', async (done) => {
-    const user = await createUser();
-    console.log('The user object', user);
+  it('should return a status 200', (done) => {
     chai.request(app)
       .get('/users')
       .end((err, res) => {
         expect(res).to.have.status(200);
-        expect(res.body).to.eql(user); // Use eql for deep equality
         done();
       });
   });
+
+  it('should return an array', done => {
+    chai.request(app)
+    .get('/users')
+    .end((err, res) => {
+      expect(res).to.be.a('object');
+      done();
+    })
+  })
 
   // Add more test cases as needed
 });
